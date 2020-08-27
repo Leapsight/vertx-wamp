e8b0b1aac058ba731752d536752d753e1547f429
Manuel Diomeda <manuel.diomeda@leapsight.com>
2020-08-27 09:28:01 -0300

    [MAG-5683, MAG-5729] - Desarrollo librería base vertx-wamp para mservices Java

---
+++ src/main/java/leapsight/vertxwamp/exceptions/GenericErrorCodes.java
@@ -0,0 +1,17 @@
+package leapsight.vertxwamp.exceptions;
+
+/**
+ * The generic error codes
+ */
+public interface GenericErrorCodes {
+
+	public final static String MISSING_REQUIRED_PARAMETERS = "com.error.missing_required_parameters";
+
+	public final static String UNEXPECTED_ERROR = "com.error.unexpected_error";
+
+	public final static String INVALID_PARAMETERS = "com.error.invalid_parameters";
+
+	public final static String NO_DATA_FOUND = "com.error.no_data_found";
+
+
+}
