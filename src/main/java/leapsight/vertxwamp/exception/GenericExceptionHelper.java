84f92edbff88706b2619debe74a46bd509a2cb77
Alejandro Miguez <alejandro.miguez@gmail.com>
2020-09-15 17:55:08 -0300

    Fixes for wamp subscriptions

--- src/main/java/leapsight/vertxwamp/exception/GenericExceptionHelper.java
+++ src/main/java/leapsight/vertxwamp/exception/GenericExceptionHelper.java
@@ -8,13 +8,16 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import jawampa.ApplicationError;
 
-public class GenericExceptionHelper {
+public final class GenericExceptionHelper {
 
-	public <T extends Exception> ApplicationError createMsgFromException(T e, String message) {
+	private GenericExceptionHelper() {
+	}
+
+	public static <T extends Exception> ApplicationError createMsgFromException(T e, String message) {
 		return createMsgFromException(GenericErrorCodes.UNEXPECTED_ERROR, message, e);
 	}
 
-	public <T extends Exception> ApplicationError createMsgFromException(String errorCode, String message, T e) {
+	public static <T extends Exception> ApplicationError createMsgFromException(String errorCode, String message, T e) {
 		final ArrayNode args = new ArrayNode(JsonNodeFactory.instance);
 		final ObjectNode kwArgs = new ObjectNode(JsonNodeFactory.instance);
 
@@ -25,15 +28,16 @@ public class GenericExceptionHelper {
 		return new ApplicationError(errorCode, args, kwArgs);
 	}
 
-	public <T> ApplicationError createMsgException(String errorCode, String message, String description, Map<String, T> data) {
+	public static <T> ApplicationError createMsgException(String errorCode, String message, String description,
+			Map<String, T> data) {
 		final ArrayNode args = new ArrayNode(JsonNodeFactory.instance);
 		final ObjectNode kwArgs = new ObjectNode(JsonNodeFactory.instance);
-		
+
 		args.add(message);
 		args.add(description);
-		if(data != null)
+		if (data != null)
 			data.entrySet().forEach(map -> kwArgs.put(map.getKey(), map.getValue().toString()));
-		
+
 		return new ApplicationError(errorCode, args, kwArgs);
 	}
 
--- src/main/java/leapsight/vertxwamp/processor/AbstractEndPointProcessor.java
+++ src/main/java/leapsight/vertxwamp/processor/AbstractEndPointProcessor.java
@@ -6,20 +6,19 @@ import java.time.temporal.ChronoUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.vertx.core.Vertx;
 import jawampa.Request;
 import leapsight.vertxwamp.util.LoggingConstants;
-import rx.functions.Action2;
+import rx.functions.Action1;
 
 /**
  * Our Processors needs to extends from this class
  */
-public abstract class AbstractEndPointProcessor implements Action2<Request, Vertx> {
+public abstract class AbstractEndPointProcessor implements Action1<Request> {
 
 	protected static final Logger LOGGER = LoggerFactory.getLogger(AbstractEndPointProcessor.class);
 
 	@Override
-	public void call(Request request, Vertx vertx) {
+	public void call(Request request) {
 		final Instant start = Instant.now();
 		final String procedure = request.details().get(LoggingConstants.WAMP_PROCEDURE_KEY) != null
 				? request.details().get(LoggingConstants.WAMP_PROCEDURE_KEY).asText()
@@ -29,7 +28,7 @@ public abstract class AbstractEndPointProcessor implements Action2<Request, Vert
 		try {
 			LOGGER.info("{} START! Request Arguments: {} - KwArguments: {}", procedure, request.arguments(),
 					request.keywordArguments());
-			process(request, vertx);
+			process(request);
 		} catch (Exception e) {
 			LOGGER.error("{} ERROR! Exception ocurred", procedure);
 		} finally {
@@ -40,6 +39,6 @@ public abstract class AbstractEndPointProcessor implements Action2<Request, Vert
 	/**
 	 * Template Method for function registration
 	 */
-	protected abstract void process(Request request, Vertx vertx);
+	protected abstract void process(Request request);
 
 }
