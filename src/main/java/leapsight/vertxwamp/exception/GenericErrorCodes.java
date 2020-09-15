344978acf7927159f6d2e890a77841ea8072ee83
Alejandro Miguez <alejandro.miguez@gmail.com>
2020-09-15 16:49:03 -0300

    Pair revision

--- src/main/java/leapsight/vertxwamp/exception/GenericErrorCodes.java
+++ src/main/java/leapsight/vertxwamp/exception/GenericErrorCodes.java
@@ -5,13 +5,12 @@ package leapsight.vertxwamp.exception;
  */
 public interface GenericErrorCodes {
 
-	public final static String MISSING_REQUIRED_PARAMETERS = "com.error.missing_required_parameters";
+	public final static String MISSING_REQUIRED_PARAMETERS = "com.magenta.error.missing_required_parameters";
 
-	public final static String UNEXPECTED_ERROR = "com.error.unexpected_error";
+	public final static String UNEXPECTED_ERROR = "com.magenta.error.unexpected_error";
 
-	public final static String INVALID_PARAMETERS = "com.error.invalid_parameters";
+	public final static String INVALID_PARAMETERS = "com.magenta.error.invalid_parameters";
 
-	public final static String NO_DATA_FOUND = "com.error.no_data_found";
-
+	public final static String NO_DATA_FOUND = "com.magenta.error.no_data_found";
 
 }
--- src/main/java/leapsight/vertxwamp/util/LoggingConstants.java
+++ src/main/java/leapsight/vertxwamp/util/LoggingConstants.java
@@ -2,32 +2,23 @@ package leapsight.vertxwamp.util;
 
 public interface LoggingConstants {
 
-    public final static String TRACE_ID_KEY = "trace_id";
+	public final static String WAMP_PROCEDURE_KEY = "procedure";
+	public final static String WAMP_START_TIME_KEY = "wamp_start_time";
 
-    public final static String METHOD = "method";
+	public final static String PROCEDURE_KEY = "procedure";
+	
+	public final static String START_DATE_KEY = "start_date";
 
-    public final static String ORIGIN = "origin";
+	public final static String END_DATE_KEY = "end_date";
 
-    public final static String PROCEDURE = "procedure";
+	public final static String TIME_ELAPSED_IN_MS_KEY = "time_elapsed_in_ms";
 
-    public final static String PATH = "path";
+	public final static String REQUEST_ARGS_KEY = "request_args";
 
-    public final static String STATUS_CODE = "status_code";
+	public final static String REQUEST_KWARGS_KEY = "request_kwargs";
 
-    public final static String REASON_PHRASE = "reason_phrase";
+	public final static String RESPONSE_KEY = "response";
 
-    public final static String START_DATE = "start_date";
-
-    public final static String END_DATE = "end_date";
-
-    public final static String TIME_ELAPSED_IN_MS = "time_elapsed_in_ms";
-
-    public final static String REQUEST = "request";
-
-    public final static String RESPONSE = "response";
-
-    public final static String STACK_TRACE = "stack_trace";
-
-    public final static String LOG_TYPE = "log_type";
+	public final static String LOG_TYPE_KEY = "log_type";
 
 }
